eebc07cfab9868f818dc87933d903ecf972191f0
rauhul <rauhul.varma@gmail.com>
2018-02-24 16:54:09 -0600

    physical memory patch

--- mp2/physical_memory.sv
+++ mp2/physical_memory.sv
@@ -1,5 +1,4 @@
-module physical_memory
-(
+module physical_memory (
     wishbone.slave wb
 );
 
@@ -8,70 +7,81 @@ timeprecision 1ns;
 
 parameter DELAY_MEM = 200;
 
-logic [127:0] mem [0:(2**12)-1];
-logic [11:0] internal_address;
-logic [127:0] data;
-logic ready, resp;
+logic [127:0] mem             [0:(2**12)-1];
+logic [ 11:0] internal_address             ;
+logic [127:0] data, wdata;
+logic         ready, resp;
 
 /* Initialize memory contents from memory.lst file */
 initial
-begin
-    $readmemh("memory.lst", mem);
-end
+    begin
+        $readmemh("memory.lst", mem);
+    end
 
-assign wb.ACK = resp;
-assign wb.RTY = wb.STB & wb.CYC & (!resp);
+assign wb.ACK   = resp;
+assign wb.RTY   = wb.STB & wb.CYC & (!resp);
 assign wb.DAT_S = data;
 
 enum int unsigned {
     idle,
     busy,
-    respond
+    respond,
+    strobe
 } state, next_state;
 
-always @(posedge wb.CLK)
-begin
+always_comb begin
+    for (int i = 0; i < 16; i++)
+        wdata[i*8+:8] = wb.SEL[i]?wb.DAT_M[i*8+:8] : mem[internal_address][i*8 +:8];
+
     /* Default */
-    resp = 1'b0;
-    data = 128'bX;
-
+    data       = 128'bX;
     next_state = state;
-
+    resp       = 1'b0;
     case(state)
-        idle: begin
-            if (wb.CYC & wb.STB) begin
+        idle : begin
+            if (wb.CYC & wb.STB)
                 next_state = busy;
-                internal_address = wb.ADR[15:4];
-                ready <= #DELAY_MEM 1;
-            end
         end
-
-        busy: begin
-            if (ready == 1) begin
+        busy : begin
+            if (ready == 1)
                 next_state = respond;
-            end
         end
-
-        respond: begin
+        respond : begin
+            next_state = strobe;
+            resp       = 1'b1;
             //bad things happen if you change the address in the middle of the transaction
-            if (wb.ADR[15:4] == internal_address) begin
-                if (wb.WE) begin
-                    mem[internal_address] = wb.DAT_M;
-                end else begin
-                    data = mem[internal_address];
-                end
-                resp = 1;
-            end
-            ready <= 0;
-            next_state = idle;
+            if ((wb.ADR == internal_address) && (!wb.WE))
+                data = mem[internal_address];
         end
-
-        default: next_state = idle;
+        strobe : begin
+            if ((wb.STB == 0) || (wb.CYC == 0))
+                next_state = idle;
+        end
+        default : next_state = idle;
     endcase
 end
 
-always_ff @(posedge wb.CLK)
-begin : next_state_assignment
+always @(posedge wb.CLK) begin
+    case(state)
+        idle : begin
+            if (wb.CYC & wb.STB) begin
+                internal_address <= wb.ADR;
+                ready            <= #DELAY_MEM 1;
+            end
+        end
+
+        respond : begin
+            //bad things happen if you change the address in the middle of the transaction
+            if ((wb.ADR == internal_address) && wb.WE)
+                mem[internal_address] <= wdata;
+            ready <= 0;
+        end
+
+        default : ;
+    endcase
+end
+
+always_ff @(posedge wb.CLK) begin : next_state_assignment
     state <= next_state;
 end
 
