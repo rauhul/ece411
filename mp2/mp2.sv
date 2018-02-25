9f57b6da7b00c402a191515e85c23bf6ebac979b
Rauhul Varma <rvarma2@siebl-0220-03.ews.illinois.edu>
2018-02-24 21:15:10 -0600

    cp3 done

--- mp2/mp2.sv
+++ mp2/mp2.sv
@@ -5,21 +5,21 @@ module mp2
     wishbone.master memory_wishbone
 );
 
-// wishbone cache_wishbone(memory_wishbone.CLK);
+wishbone cache_wishbone(memory_wishbone.CLK);
 
 cpu _cpu
 (
     /* CPU <-> Cache */
-    .memory_wishbone//(cache_wishbone)
+    .memory_wishbone(cache_wishbone)
 );
 
-// cache _cache
-// (
-//     /* CPU <-> Cache */
-//     .cpu_wishbone(cache_wishbone),
+cache _cache
+(
+    /* CPU <-> Cache */
+    .cpu_wishbone(cache_wishbone),
 
-//     /* Cache <-> Memory */
-//     .memory_wishbone
-// );
+    /* Cache <-> Memory */
+    .memory_wishbone
+);
 
 endmodule : mp2
