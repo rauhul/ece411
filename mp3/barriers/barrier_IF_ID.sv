6da2108858e87f62afb4e6829653219cbdb5f221
rauhul <rauhul.varma@gmail.com>
2018-03-10 00:04:03 -0600

    inital barrier_IF_ID

--- mp3/barriers/barrier_IF_ID.sv
+++ mp3/barriers/barrier_IF_ID.sv
@@ -1,8 +1,31 @@
 module barrier_IF_ID (
     /* INPUTS */
-    input clk
+    input clk,
+    input lc3b_word pc_in,
+    input lc3b_word ir_in,
 
     /* OUTPUTS */
+    output lc3b_word pc_out,
+    output lc3b_word ir_out
 );
+
+lc3b_word pc;
+lc3b_word ir;
+
+/* INITIAL */
+initial begin
+    pc = 16'b0;
+    ir = 16'b0;
+end
+
+/* CLOCK */
+always_ff @(posedge clk) begin
+    pc = pc_in;
+    ir = ir_in;
+end
+
+/* ALWAYS */
+assign pc_out = pc;
+assign ir_out = ir;
 
 endmodule: barrier_IF_ID
