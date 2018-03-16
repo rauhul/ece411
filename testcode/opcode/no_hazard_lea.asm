f681bed9a7eb7726f76e4f9f263fc2d524d852bf
rauhul <rauhul.varma@gmail.com>
2018-03-16 15:07:25 -0500

    update testcases

--- testcode/opcode/no_hazard_lea.asm
+++ testcode/opcode/no_hazard_lea.asm
@@ -1,12 +1,12 @@
 SEGMENT  CodeSegment:
 
-    LEA  R1, T_1 ; R1 = 0x0d
-    LEA  R2, T_2 ; R2 = 0x10
-    LEA  R3, T_3 ; R3 = 0x12
-    LEA  R4, T_4 ; R4 = 0x14
-    LEA  R5, T_5 ; R5 = 0x16
-    LEA  R6, T_6 ; R6 = 0x18
-    LEA  R7, T_7 ; R7 = 0x1a
+     LEA  R1, T_1 ; R1 = 0x0e
+     LEA  R2, T_2 ; R2 = 0x10
+     LEA  R3, T_3 ; R3 = 0x12
+     LEA  R4, T_4 ; R4 = 0x14
+     LEA  R5, T_5 ; R5 = 0x16
+     LEA  R6, T_6 ; R6 = 0x18
+     LEA  R7, T_7 ; R7 = 0x1a
 
 T_1: NOP
 T_2: NOP
