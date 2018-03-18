5b2b4f20a7bc42de3ddaaae38590c300aba3f786
rauhul <rauhul.varma@gmail.com>
2018-03-18 14:50:02 -0500

    re-add barriers to arbiter, add annotated mp3cp2a testcode

--- mp3/cache/cache_arbiter.sv
+++ mp3/cache/cache_arbiter.sv
@@ -12,74 +12,74 @@ module cache_arbiter (
 logic clk;
 assign clk = output_wishbone.CLK;
 
-// logic [127:0] input_wishbone0_DAT_M;
-// logic input_wishbone0_CYC;
-// logic input_wishbone0_STB;
-// logic input_wishbone0_WE;
-// logic [15:0] input_wishbone0_SEL;
-// logic [11:0] input_wishbone0_ADR;
+logic [127:0] input_wishbone0_DAT_M;
+logic input_wishbone0_CYC;
+logic input_wishbone0_STB;
+logic input_wishbone0_WE;
+logic [15:0] input_wishbone0_SEL;
+logic [11:0] input_wishbone0_ADR;
 
-// barrier_wishbone_master _input_wishbone0 (
-//     /* INPUTS */
-//     .clk,
-//     .DAT_M_in(input_wishbone0.DAT_M),
-//     .CYC_in(input_wishbone0.CYC),
-//     .STB_in(input_wishbone0.STB),
-//     .WE_in(input_wishbone0.WE),
-//     .SEL_in(input_wishbone0.SEL),
-//     .ADR_in(input_wishbone0.ADR),
+barrier_wishbone_master _input_wishbone0 (
+    /* INPUTS */
+    .clk,
+    .DAT_M_in(input_wishbone0.DAT_M),
+    .CYC_in(input_wishbone0.CYC),
+    .STB_in(input_wishbone0.STB),
+    .WE_in(input_wishbone0.WE),
+    .SEL_in(input_wishbone0.SEL),
+    .ADR_in(input_wishbone0.ADR),
 
-//     /* OUTPUTS */
-//     .DAT_M_out(input_wishbone0_DAT_M),
-//     .CYC_out(input_wishbone0_CYC),
-//     .STB_out(input_wishbone0_STB),
-//     .WE_out(input_wishbone0_WE),
-//     .SEL_out(input_wishbone0_SEL),
-//     .ADR_out(input_wishbone0_ADR)
-// );
+    /* OUTPUTS */
+    .DAT_M_out(input_wishbone0_DAT_M),
+    .CYC_out(input_wishbone0_CYC),
+    .STB_out(input_wishbone0_STB),
+    .WE_out(input_wishbone0_WE),
+    .SEL_out(input_wishbone0_SEL),
+    .ADR_out(input_wishbone0_ADR)
+);
 
-// logic [127:0] input_wishbone1_DAT_M;
-// logic input_wishbone1_CYC;
-// logic input_wishbone1_STB;
-// logic input_wishbone1_WE;
-// logic [15:0] input_wishbone1_SEL;
-// logic [11:0] input_wishbone1_ADR;
+logic [127:0] input_wishbone1_DAT_M;
+logic input_wishbone1_CYC;
+logic input_wishbone1_STB;
+logic input_wishbone1_WE;
+logic [15:0] input_wishbone1_SEL;
+logic [11:0] input_wishbone1_ADR;
 
-// barrier_wishbone_master _input_wishbone1 (
-//     /* INPUTS */
-//     .clk,
-//     .DAT_M_in(input_wishbone1.DAT_M),
-//     .CYC_in(input_wishbone1.CYC),
-//     .STB_in(input_wishbone1.STB),
-//     .WE_in(input_wishbone1.WE),
-//     .SEL_in(input_wishbone1.SEL),
-//     .ADR_in(input_wishbone1.ADR),
+barrier_wishbone_master _input_wishbone1 (
+    /* INPUTS */
+    .clk,
+    .DAT_M_in(input_wishbone1.DAT_M),
+    .CYC_in(input_wishbone1.CYC),
+    .STB_in(input_wishbone1.STB),
+    .WE_in(input_wishbone1.WE),
+    .SEL_in(input_wishbone1.SEL),
+    .ADR_in(input_wishbone1.ADR),
 
-//     /* OUTPUTS */
-//     .DAT_M_out(input_wishbone1_DAT_M),
-//     .CYC_out(input_wishbone1_CYC),
-//     .STB_out(input_wishbone1_STB),
-//     .WE_out(input_wishbone1_WE),
-//     .SEL_out(input_wishbone1_SEL),
-//     .ADR_out(input_wishbone1_ADR)
-// );
+    /* OUTPUTS */
+    .DAT_M_out(input_wishbone1_DAT_M),
+    .CYC_out(input_wishbone1_CYC),
+    .STB_out(input_wishbone1_STB),
+    .WE_out(input_wishbone1_WE),
+    .SEL_out(input_wishbone1_SEL),
+    .ADR_out(input_wishbone1_ADR)
+);
 
-// logic [127:0] output_wishbone_DAT_S;
-// logic output_wishbone_ACK;
-// logic output_wishbone_RTY;
+logic [127:0] output_wishbone_DAT_S;
+logic output_wishbone_ACK;
+logic output_wishbone_RTY;
 
-// barrier_wishbone_slave _output_wishbone (
-//     /* INPUTS */
-//     .clk,
-//     .DAT_S_in(output_wishbone.DAT_S),
-//     .ACK_in(output_wishbone.ACK),
-//     .RTY_in(output_wishbone.RTY),
+barrier_wishbone_slave _output_wishbone (
+    /* INPUTS */
+    .clk,
+    .DAT_S_in(output_wishbone.DAT_S),
+    .ACK_in(output_wishbone.ACK),
+    .RTY_in(output_wishbone.RTY),
 
-//     /* OUTPUTS */
-//     .DAT_S_out(output_wishbone_DAT_S),
-//     .ACK_out(output_wishbone_ACK),
-//     .RTY_out(output_wishbone_RTY)
-// );
+    /* OUTPUTS */
+    .DAT_S_out(output_wishbone_DAT_S),
+    .ACK_out(output_wishbone_ACK),
+    .RTY_out(output_wishbone_RTY)
+);
 
 /* input_wishbone0 has priority */
 enum int unsigned {
@@ -117,15 +117,15 @@ always_comb begin : owner_actions
         end
 
         o_input0: begin
-            output_wishbone.DAT_M = input_wishbone0.DAT_M;
-            output_wishbone.CYC   = input_wishbone0.CYC;
-            output_wishbone.STB   = input_wishbone0.STB;
-            output_wishbone.WE    = input_wishbone0.WE;
-            output_wishbone.SEL   = input_wishbone0.SEL;
-            output_wishbone.ADR   = input_wishbone0.ADR;
+            output_wishbone.DAT_M = input_wishbone0_DAT_M;
+            output_wishbone.CYC   = input_wishbone0_CYC;
+            output_wishbone.STB   = input_wishbone0_STB;
+            output_wishbone.WE    = input_wishbone0_WE;
+            output_wishbone.SEL   = input_wishbone0_SEL;
+            output_wishbone.ADR   = input_wishbone0_ADR;
 
-            input_wishbone0.DAT_S = output_wishbone.DAT_S;
-            input_wishbone0.ACK   = output_wishbone.ACK;
+            input_wishbone0.DAT_S = output_wishbone_DAT_S;
+            input_wishbone0.ACK   = output_wishbone_ACK;
             input_wishbone0.RTY   = 0;
 
             input_wishbone1.DAT_S = 128'bx;
@@ -134,19 +134,19 @@ always_comb begin : owner_actions
         end
 
         o_input1: begin
-            output_wishbone.DAT_M = input_wishbone1.DAT_M;
-            output_wishbone.CYC   = input_wishbone1.CYC;
-            output_wishbone.STB   = input_wishbone1.STB;
-            output_wishbone.WE    = input_wishbone1.WE;
-            output_wishbone.SEL   = input_wishbone1.SEL;
-            output_wishbone.ADR   = input_wishbone1.ADR;
+            output_wishbone.DAT_M = input_wishbone1_DAT_M;
+            output_wishbone.CYC   = input_wishbone1_CYC;
+            output_wishbone.STB   = input_wishbone1_STB;
+            output_wishbone.WE    = input_wishbone1_WE;
+            output_wishbone.SEL   = input_wishbone1_SEL;
+            output_wishbone.ADR   = input_wishbone1_ADR;
 
             input_wishbone0.DAT_S = 128'bx;
             input_wishbone0.ACK   = 0;
             input_wishbone0.RTY   = 1;
 
-            input_wishbone1.DAT_S = output_wishbone.DAT_S;
-            input_wishbone1.ACK   = output_wishbone.ACK;
+            input_wishbone1.DAT_S = output_wishbone_DAT_S;
+            input_wishbone1.ACK   = output_wishbone_ACK;
             input_wishbone1.RTY   = 0;
         end
 
--- testcode/checkpoint/mp3cp2a.asm
+++ testcode/checkpoint/mp3cp2a.asm
@@ -10,28 +10,28 @@ DSP:    DATA2 DataSeg
     ; cache line boundary
 
 ldr_str_test:
-    LDR R0, R0, DSP
+    LDR R0, R0, DSP ; 0x0080
     NOP
     NOP
     NOP
     NOP
     NOP
-    LDR R1, R0, W
-    LDR R3, R0, Y
+    LDR R1, R0, W ; 0x0009
+    LDR R3, R0, Y ; 0x0001
     ; cache line boundary
-    LDR R2, R0, X
-    LDR R4, R0, Z
+    LDR R2, R0, X ; 0x0002
+    LDR R4, R0, Z ; 0x0003
     NOP
-    STR R1, R0, Z
-    STR R2, R0, Y
-    STR R3, R0, X
-    STR R4, R0, W
+    STR R1, R0, Z ; store 0x0009
+    STR R2, R0, Y ; store 0x0002
+    STR R3, R0, X ; store 0x0001
+    STR R4, R0, W ; store 0x0003
     NOP
     ; cache line boundary
-    LDR R1, R0, W
-    LDR R2, R0, X
-    LDR R3, R0, Y
-    LDR R4, R0, Z
+    LDR R1, R0, W ; 0x0003
+    LDR R2, R0, X ; 0x0001
+    LDR R3, R0, Y ; 0x0002
+    LDR R4, R0, Z ; 0x0009
     BRnzp fetch_stall_test
     NOP
     NOP
@@ -54,30 +54,30 @@ ldr_str_test:
     NOP
     NOP
 fetch_stall_test:
-    ADD R5, R1, R2
+    ADD R5, R1, R2 ; 0x0004
     ; cache line boundary
-    ADD R6, R3, R4
+    ADD R6, R3, R4 ; 0x000b
     NOP
     NOP
     NOP
     NOP
-    STR R5, R0, VICTIM
-    ADD R7, R5, R6
+    STR R5, R0, VICTIM ; store 0x0004
+    ADD R7, R5, R6 ; 0x000f
     NOP
     ; cache line boundary
     NOP
     NOP
     NOP
-    STR R7, R0, TOTAL
-    LDR R1, R0, TOTAL
+    STR R7, R0, TOTAL ; store 0x000f ; < wb
+    LDR R1, R0, TOTAL ; 0x000f ; < mem
 inf:
-    BRnzp inf
-    NOP
-    NOP
+    BRnzp inf ; < ex
+    NOP ; < id
+    NOP ; < if
 
 ; cache line boundary
 SEGMENT DataSeg:
-NOP
+NOP ; < if_n
 NOP
 NOP
 NOP
