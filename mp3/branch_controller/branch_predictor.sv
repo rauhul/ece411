d30b2c29ebf3ec7dfd81f28a178a4c7423071823
rauhul <rauhul.varma@gmail.com>
2018-04-20 02:24:21 -0500

    revert to static predictor

--- mp3/branch_controller/branch_predictor.sv
+++ mp3/branch_controller/branch_predictor.sv
@@ -12,92 +12,83 @@ module branch_predictor (
     output logic prediction
 );
 
-enum int unsigned {
-    /* List of states */
-    s_taken_2,
-    s_taken,
-    s_ntaken,
-    s_ntaken_2
-} state, next_state;
+assign prediction = 0;
 
-always_comb begin : state_actions
-    /* Default output assignments */
-    prediction = 'x;
+// enum int unsigned {
+//     /* List of states */
+//     s_taken_2,
+//     s_taken,
+//     s_ntaken,
+//     s_ntaken_2
+// } state, next_state;
 
-    /* Actions for each state */
-     case(state)
-        s_taken_2: begin
-            prediction = 1;
-        end
+// always_comb begin : state_actions
+//     /* Default output assignments */
+//     prediction = 'x;
 
-        s_taken: begin
-            prediction = 1;
-        end
+//     /* Actions for each state */
+//      case(state)
+//         s_taken_2:  prediction = 1;
+//         s_taken:    prediction = 1;
+//         s_ntaken:   prediction = 0;
+//         s_ntaken_2: prediction = 0;
+//     endcase
+// end
 
-        s_ntaken: begin
-            prediction = 0;
-        end
+// always_comb begin : next_state_logic
+//     /* Default next state assignment */
+//     next_state = state;
 
-        s_ntaken_2: begin
-            prediction = 0;
-        end
-    endcase
-end
+//     /* Next state information and conditions (if any)
+//      * for transitioning between states */
+//     case(state)
+//         s_taken_2: begin
+//             if (update) begin
+//                 if (update_value) begin
+//                     next_state = s_taken_2;
+//                 end else begin
+//                     next_state = s_taken;
+//                 end
+//             end
+//         end
 
-always_comb begin : next_state_logic
-    /* Default next state assignment */
-    next_state = state;
+//         s_taken: begin
+//             if (update) begin
+//                 if (update_value) begin
+//                     next_state = s_taken_2;
+//                 end else begin
+//                     next_state = s_ntaken;
+//                 end
+//             end
+//         end
 
-    /* Next state information and conditions (if any)
-     * for transitioning between states */
-    case(state)
-        s_taken_2: begin
-            if (update) begin
-                if (update_value) begin
-                    next_state = s_taken_2;
-                end else begin
-                    next_state = s_taken;
-                end
-            end
-        end
+//         s_ntaken: begin
+//             if (update) begin
+//                 if (update_value) begin
+//                     next_state = s_taken;
+//                 end else begin
+//                     next_state = s_ntaken_2;
+//                 end
+//             end
+//         end
 
-        s_taken: begin
-            if (update) begin
-                if (update_value) begin
-                    next_state = s_taken_2;
-                end else begin
-                    next_state = s_ntaken;
-                end
-            end
-        end
+//         s_ntaken_2: begin
+//             if (update) begin
+//                 if (update_value) begin
+//                     next_state = s_ntaken;
+//                 end else begin
+//                     next_state = s_ntaken_2;
+//                 end
+//             end
+//         end
+//     endcase // state
+// end
 
-        s_ntaken: begin
-            if (update) begin
-                if (update_value) begin
-                    next_state = s_taken;
-                end else begin
-                    next_state = s_ntaken_2;
-                end
-            end
-        end
-
-        s_ntaken_2: begin
-            if (update) begin
-                if (update_value) begin
-                    next_state = s_ntaken;
-                end else begin
-                    next_state = s_ntaken_2;
-                end
-            end
-        end
-    endcase // state
-end
-
-always_ff @(posedge clk) begin: next_state_assignment
-    /* Assignment of next state on clock edge */
-    if (~stall) begin
-        state <= next_state;
-    end
-end
+// always_ff @(posedge clk) begin: next_state_assignment
+//     /* Assignment of next state on clock edge */
+//     if (~stall) begin
+//         state <= next_state;
+//     end
+// end
 
 endmodule : branch_predictor
