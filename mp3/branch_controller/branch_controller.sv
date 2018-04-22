58536f4ec42815e230c404d28e491f36fc095688
rauhul <rauhul.varma@gmail.com>
2018-04-21 23:26:52 -0500

    fix parameter name

--- mp3/branch_controller/branch_controller.sv
+++ mp3/branch_controller/branch_controller.sv
@@ -93,7 +93,7 @@ branch_waterfall_queue _branch_waterfall_queue (
 
 branch_predictor #(
     .BRANCH_HISTORY_REGISTER_SIZE(4),
-    .NUM_INDEXING_PC_BITS(1)
+    .NUM_PHT_INDEXING_PC_BITS(1)
 ) _branch_predictor (
     /* INPUTS */
     .clk,
